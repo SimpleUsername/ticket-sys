119a05523e225c509a1a3d9b8cc5f86894798e9c
il-marc <il-marc@ya.ru>
2014-06-28 01:26:33 +0300

    [UI] tickets multiselect feature

--- application/views/tickets_sell_modal_view.php
+++ application/views/tickets_sell_modal_view.php
@@ -13,9 +13,16 @@
 <select size="1" id="place" class="form-control" multiple>
     <option>Сначала выберите ряд</option>
 </select>
-<h4>Билетов: 0</h4>
-<h4>Итого: 0 грн.</h4>
+<div class="text-right">
+    <h4>Билеты: </h4>
+    <div id="tickets">
+
+    </div>
+    <h4>Итого: <b id="total">0</b> грн.</h4>
+</div>
 <script>
+    var tickets = [];
+    var prices = [];
 
     $("#row").html('').hide();
     $("#place").html('').hide();
@@ -57,9 +64,12 @@
             var arr = $.parseJSON(response);
             $("#place").fadeIn().attr("size", arr.length).html("");
             for (var i=0; i<arr.length; i++) {
-                var disabledAttr = ' ';
+                var disabledAttr = '';
+                var selectedAttr = '';
                 var color;
-                // style="color:gray"
+                if (tickets.indexOf(parseInt(arr[i]['place_id'])) > -1) {
+                    selectedAttr = "selected";
+                }
                 if (arr[i]['ticket_type'] == 'purchased') {
                     disabledAttr = "disabled";
                     color = "Snow";
@@ -69,8 +79,9 @@
                 } else {
                     color = "#dff0d8";
                 }
-                $("#place").append("<option "+disabledAttr+
+                $("#place").append("<option "+disabledAttr+" "+selectedAttr+
                     " style='background-color:"+color+"' data-place-id="+ arr[i]['place_id'] +
+                    " data-place-no="+ arr[i]['place_no'] +
                     ">Место "+arr[i]['place_no']+"</option>");
                 if (arr[i]['ticket_type'] == 'purchased') {
                     $("[data-place-id="+ arr[i]['place_id']+"]").append(" - продано");
@@ -81,8 +92,46 @@
             $("#row").removeAttr("disabled").attr("size", 1);
         });
     });
+    $("#tickets").on("click", "span.ticket", function (event) {
+        console.log(event);
+        var placeId = $(event.target).data('placeId');
+        var index = tickets.indexOf(placeId);
+        $(event.target).remove();
+        $("#total").html(parseInt($("#total").html())-prices[index]);
+        prices.splice(index, 1);
+        tickets.splice(index, 1);
+        $("option[data-place-id="+ placeId +"]").attr("selected", false);
+        if (tickets.length > 0) {
+            $("#btn-modal-confirm").removeClass("disabled");
+        } else {
+            $("#btn-modal-confirm").addClass("disabled");
+        }
+    });
     $("#place").change(function() {
-        if ($('#place option:selected').length > 0) {
+        $.each($('#place option'), function(index, option) {
+            if ($(option).is(':selected') == true) {
+                if (tickets.indexOf($(option).data('placeId')) == -1) {
+                    tickets.push($(option).data('placeId'));
+                    prices.push($("#sector option:selected").data('sectorPrice'));
+                    $("#total").html(parseInt($("#total").html())+prices[prices.length-1]);
+                    $('#tickets').append("<span title='Удалить' style='cursor: pointer' class='label label-primary ticket'" +
+                        " data-place-id="+$(option).data('placeId')+">С-"+$('#sector option:selected').data('sectorId')+
+                        " Р-"+$("#row option:selected").data('rowNo')+
+                        " М-"+$(option).data('placeNo')+" &times;</span> ");
+                }
+            } else {
+                if (tickets.indexOf($(option).data('placeId')) > -1) {
+                    var placeId = $("span[data-place-id="+ $(option).data('placeId') +"]").data('placeId');
+                    var i = tickets.indexOf(placeId);
+                    $("span[data-place-id="+ $(option).data('placeId') +"]").remove();
+                    $("#total").html(parseInt($("#total").html())-prices[i]);
+                    prices.splice(i, 1);
+                    tickets.splice(i, 1);
+                }
+            }
+        });
+
+        if (tickets.length > 0) {
             $("#btn-modal-confirm").removeClass("disabled");
         } else {
             $("#btn-modal-confirm").addClass("disabled");
