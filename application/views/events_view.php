5c2b26a68becde806a348b1d16dfec00ad304903
il-marc <il-marc@ya.ru>
2014-06-28 22:07:21 +0300

    sell and booking buttons...
    ... disable, when selling and booking not allowed

--- application/views/events_view.php
+++ application/views/events_view.php
@@ -1,18 +1,28 @@
 <h1>События</h1>
+<?
+date_default_timezone_set('Europe/Kiev');
+$current_date = time();
+?>
+<? if(isset($data['msg'])) {
+?>
+    <p><?=$data['msg']?></p>
+<? } else { ?>
 <div class="table-responsive">
     <table class="table">
-        <th>Превью</th>
-        <th>Название</th>
-        <th>Статус</th>
-        <th>Дата события</th>
-        <th>Старт бронирования</th>
-        <th>Старт продаж</th>
-        <? if ($_SESSION['user_type_id'] == 2) { ?>
-        <th>Редактировать</th>
-        <th>Удалить</th>
-        <? } elseif ($_SESSION['user_type_id'] == 3) { ?>
-        <th>Продать</th>
-        <th>Забронировать</th>
+        <tr>
+            <th>Превью</th>
+            <th>Название</th>
+            <th>Статус</th>
+            <th>Дата события</th>
+            <th>Старт бронирования</th>
+            <th>Старт продаж</th>
+            <? if ($_SESSION['user_type_id'] == 2) { ?>
+            <th>Редактировать</th>
+            <th>Удалить</th>
+            <? } elseif ($_SESSION['user_type_id'] == 3) { ?>
+            <th>Продать</th>
+            <th>Забронировать</th>
+        </tr>
         <? } ?>
         <? foreach($data as $id => $value){?>
             <? if($id%2 == 0){?>
@@ -24,21 +34,21 @@
             <? } ?>
                     <td><?=$value['event_name']?></td>
                     <td><?=$value['estatus_name']?></td>
-                    <td><?=$value['event_date']?></td>
-                    <td><?=$value['event_booking']?></td>
-                    <td><?=$value['event_sale']?></td>
+                    <td><?=$value['event_date']?><? $event_date = strtotime($value['event_date']); ?></td>
+                    <td><?=$value['event_booking']?><? $event_booking = strtotime($value['event_booking']); ?></td>
+                    <td><?=$value['event_sale']?><? $event_sale = strtotime($value['event_sale']); ?></td>
                     <? if ($_SESSION['user_type_id'] == 2) { ?>
                     <td><a class="btn btn-success" href="/events/edit/<?=$value['event_id']?>">Редактировать</a></td>
                     <td><button class="btn btn-danger" id="del_ev" onclick="confirm('Удалить ?')" data-event_id="<?=$value['event_id']?>">Удалить</button></td>
                     <? } elseif ($_SESSION['user_type_id'] == 3) { ?>
                     <td>
-                        <a class="btn btn-primary btn-sell"
+                        <a class="btn btn-primary btn-sell <?=($event_date < $current_date || $event_sale > $current_date)?"disabled":""?>"
                            data-event-id="<?=$value['event_id']?>">
                             Продать
                         </a>
                     </td>
                     <td>
-                        <a class="btn btn-primary btn-reserve"
+                        <a class="btn btn-primary btn-reserve <?=($event_date < $current_date || $event_booking > $current_date)?"disabled":""?>"
                            data-event-id="<?=$value['event_id']?>">
                             Бронировать
                         </a>
@@ -69,5 +79,5 @@
     });
 </script>
 <? } ?>
-
+<? } ?>
 <? //echo "<pre>"; print_r($data); echo "</pre>";?>
