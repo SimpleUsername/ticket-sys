0670ea79615138f8853f545bef03dad063fb696d
il-marc <il-marc@ya.ru>
2014-07-09 01:10:07 +0300

    default action fix

--- application/controllers/controller_main.php
+++ application/controllers/controller_main.php
@@ -7,12 +7,15 @@ class Controller_Main extends Controller
 	{
         if ($_SESSION['user_seller']) {
             $this->redirect("events");
+            exit();
         }
         if ($_SESSION['user_manager']) {
             $this->redirect("events");
+            exit();
         }
         if ($_SESSION['user_admin']) {
             $this->redirect("users");
+            exit();
         }
 	}
 }
