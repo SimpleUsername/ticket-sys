264b095b96e86464875cb311e7ea263c2c41c478
il-marc <il-marc@ya.ru>
2014-07-13 14:30:39 +0300

    free places count

--- application/models/model_events.php
+++ application/models/model_events.php
@@ -11,12 +11,15 @@ class Model_Events extends Model
         if($status){
             $result = $this->db->sql("SELECT ev.*, evs.estatus_name,
                                         sum(case when t.ticket_type = 'reserved' then 1 else 0 end) reserved_count,
-                                        sum(case when t.ticket_type = 'purchased' then 1 else 0 end) purchased_count
+                                        sum(case when t.ticket_type = 'purchased' then 1 else 0 end) purchased_count,
+                                        fc.free_count
                                         FROM `{$this->_table}` as ev
                                         LEFT JOIN `event_status` as evs
                                         ON ev.event_status = evs.estatus_id
-                                        JOIN tickets as t
+                                        LEFT JOIN tickets as t
                                         ON ev.event_id = t.event_id
+                                        JOIN (select event_id, sum(free_count) free_count from tickets_count group by event_id) fc
+                                        ON ev.event_id = fc.event_id
                                         WHERE `event_status` > -1 GROUP BY ev.event_id");
 
             if(!$result){
