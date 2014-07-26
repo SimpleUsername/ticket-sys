e15bbc008133d80da401c57f36b6b6a78cda70c4
il-marc <il-marc@ya.ru>
2014-07-27 02:38:50 +0300

    -2sec

--- conf.php
+++ conf.php
@@ -2,7 +2,7 @@
 
 define('DB_USER', 'root');
 define('DB_PASS', '');
-define('DB_HOST', 'localhost');
+define('DB_HOST', '127.0.0.1');
 define('DB_NAME', 'ticket-sys');
 define('DB_DRIVER', 'mysql');
 define('SECURE_SALT', 'dataart');
