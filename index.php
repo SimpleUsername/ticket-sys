a98017304c21ef234883c668a759c0400daa3b5f
il-marc <il-marc@ya.ru>
2014-07-27 03:14:27 +0300

    xhprof

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
 ﻿<?php
+if (extension_loaded("xhprof")) {
+    xhprof_enable();
+}
+
 //error_reporting(E_ALL);
 ini_set('display_errors', 1);
 require_once 'application/bootstrap.php';
+
+if (extension_loaded("xhprof")) {
+    $xhprof_data = xhprof_disable();
+    include_once "/xhprof_lib/utils/xhprof_lib.php";
+    include_once "/xhprof_lib/utils/xhprof_runs.php";
+    $xhprof_runs = new XHProfRuns_Default();
+    $run_id = $xhprof_runs->save_run($xhprof_data, preg_replace('#(&|/)#', '_', $_SERVER['REQUEST_URI']));
+}
