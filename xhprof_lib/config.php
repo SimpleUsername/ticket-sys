d72caeb0450439133067f1f4af46c9cb5a3be7fd
Evil NoTitle <killbox@i.ua>
2014-08-02 21:18:38 +0300

    xhprof graphviz config

---
+++ xhprof_lib/config.php
@@ -0,0 +1,5 @@
+<?php
+define('ERROR_FILE', 'D:/xampp/logs/xhprof_dot_errfile.log');
+define('TMP_DIRECTORY', 'D:/xampp/tmp/xhprof');
+define('DOT_BINARY', 'D:\Program Files\Graphviz2.38\bin\dot.exe');
+
